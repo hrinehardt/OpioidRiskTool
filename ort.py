3f8acf8ad8c0ec646c2b2fb1bfc24f8812539660
hrinehardt <Hannah@Hannahs-MacBook-Air.local>
2015-01-05 10:56:57 -0500

    Adds scoring risk; moves genderAnswer to top

--- ort.py
+++ ort.py
@@ -1,9 +1,26 @@
+genderAnswer = None
 famHistAnswer = None
 personHistAnswer = None
-genderAnswer = None
 ageAnswer = None
 sxlAbuseAnswer = None
 psychDxAnswer = None
+
+
+def gender():
+  print("Please enter your gender. ")
+  choice = raw_input().lower()
+
+  male = set(['male', 'm', 'man'])
+  female = set(['female', 'f', 'woman'])
+
+  if choice in male or female:
+    genderAnswer = 'yes'
+  elif choice in male or female:
+    genderAnswer = 'no'
+  else:
+    print("Please respond with 'male' or 'female'")
+    gender()
+
 
 def famHist():
   print("Do you have a family history of substance abuse? ")
@@ -35,22 +52,6 @@ def personHist():
   else:
     print("Please respond with 'yes' or 'no'")
     personHist()
-
-
-def gender():
-  print("Please enter your gender. ")
-  choice = raw_input().lower()
-
-  male = set(['male', 'm', 'man'])
-  female = set(['female', 'f', 'woman'])
-
-  if choice in male or female:
-    genderAnswer = 'yes'
-  elif choice in male or female:
-    genderAnswer = 'no'
-  else:
-    print("Please respond with 'male' or 'female'")
-    gender()
 
 
 def age():
@@ -117,19 +118,21 @@ def askYesNo(question, responseVariable):
 
 
 
-
+gender()
 famHist()
 personHist()
-gender()
 age()
 sxlAbuse()
 psychDx()
 
+
+print(genderAnswer)
 print(famHistAnswer)
 print(personHistAnswer)
-print(genderAnswer)
 print(ageAnswer)
 print(sxlAbuseAnswer)
 print(psychDxAnswer)
 
-def calculation(data):
+
+# Adds Risk scale
+print "Risk:\n 0-3 - low \n 4-7 - moderate \n >= 8 - high"
